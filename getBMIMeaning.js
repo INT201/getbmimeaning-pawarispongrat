1845f065e098fe59902db5ac5de7553c2ad5671c
pawaris <pawaris.test@mail.kmutt.ac.th>
2022-09-04 15:21:31 +0700

    ans

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return weight/(height*height);
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let bmi = calculateBMI(weight,height)
+  if(bmi < 18.5){
+    return "underweight";
+  }else if(bmi > 18.2 && bmi <= 24.9){
+    return "Normal weight";
+  }else{
+    return"Overweight"
+  }
 }
-module.exports = getBMIMeaning
+//module.exports = getBMIMeaning
+
+console.log(calculateBMI(64,1.1));
+console.log(getBMIMeaning(64,3));
